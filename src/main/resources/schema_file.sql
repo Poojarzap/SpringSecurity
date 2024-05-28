27f2e0ac8092e940319bbbfc1df083c830026aa3
Poojarzap <[pooja.r@zapcg.com>
2024-05-28 16:39:37 +0530

    H2 database is also implemeted for user management

--- src/main/resources/schema_file.sql
+++
@@ -1,4 +0,0 @@
---h2 is in memory DB, erases data after each run, hence this file is needed. If other DBs are used, then need not create this file
-create table users(username varchar_ignorecase(50) not null primary key,password varchar_ignorecase(500) not null,enabled boolean not null);
-create table authorities (username varchar_ignorecase(50) not null,authority varchar_ignorecase(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
-create unique index ix_auth_username on authorities (username,authority);
